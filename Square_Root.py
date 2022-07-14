a51b5addb0f2894a3618cbb75a22fc804d3e3177
Mik-Gee <107235034+Mik-Gee@users.noreply.github.com>
2022-07-14 11:27:45 +1000

    From the last couple of weeks (testing to see if PUSH works)

--- Square_Root.py
+++ Square_Root.py
@@ -2,7 +2,7 @@ num=float(input(("Enter a number: ")))
 
 num_sqrt=num**0.5
 
-print("The square root of",num,"is",num_sqrt) #I want to know how to remove the '' from the result
+print"The square root of",num,"is",num_sqrt #I want to know how to remove the '' from the result
 print("The square root of %0.2f is %0.2f" %(num,num_sqrt))
 
 #Added both prints so I can see the difference#
